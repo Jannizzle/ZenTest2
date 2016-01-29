b0a21c126dfd68edd18444c9172a9457f7025cf7
Jannizzle <jannis.xenakis@gmx.de>
2016-01-29 13:54:41 +0100

    reset
    weres

--- Assets/Plugins/iOS/ZendeskStringParams.h
+++
@@ -1,6 +0,0 @@
-
-// Converts C style string to NSString
-#define GetStringParam(_x_) (_x_ != NULL) ? [NSString stringWithUTF8String:_x_] : [NSString stringWithUTF8String:""]
-
-// Converts C style string to NSString as long as it isnt empty
-#define GetStringParamOrNil(_x_) (_x_ != NULL && strlen(_x_)) ? [NSString stringWithUTF8String:_x_] : nil
